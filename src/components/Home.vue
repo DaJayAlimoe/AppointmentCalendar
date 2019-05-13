e333b3f2edb8c7758173f23a4151168b19d906dc
Ronny <ronaldsafowat.kattan@haw-hamburg.de>
2019-05-13 09:04:58 +0200

    added identifiers for event buttons

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,11 +2,11 @@
   <v-container fluid grid-list-xl>
     <v-layout row justify-space-between>
       <v-flex sm4 xs12 class="text-sm-left text-xs-center">
-        <v-btn dark small color="primary">Create</v-btn>
+        <v-btn id="create_appointment" small color="primary">Create</v-btn>
       </v-flex>
       <v-spacer></v-spacer>
       <v-flex sm4 xs12 class="text-sm-right text-xs-center">
-        <v-btn dark small color="secondary">logout</v-btn>
+        <v-btn id="user_logout" dark small color="secondary">logout</v-btn>
       </v-flex>
     </v-layout>
     <v-spacer></v-spacer>
